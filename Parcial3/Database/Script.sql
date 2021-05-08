68c5fa2e9d8b073443272a2a2bbd2d6262e3bfd4
Jesus-Antonio-Villanueva-Hidalgo <jesusantonio.ja200@gmail.com>
2021-05-07 21:13:14 -0500

    Tarea1

---
+++ Parcial3/Database/Script.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE p3_tarea1;
+USE p3_tarea1;
+
+CREATE TABLE usuario
+(
+id_usuario smallint auto_increment,
+nombre varchar(50) not null,
+apellido_paterno varchar(30) not null,
+apellido_materno varchar(30) not null,
+correo varchar(50) not null,
+contraseña varchar (30) not null,
+constraint PK_USUARIO primary key (id_usuario)
+);
+
+INSERT INTO usuario VALUES ('Jesus Antonio','Villanueva','Hidalgo','jesus@gmail.com','Blitz');
